233bc6fbe35381219aa32e7c49920895fc9f6d90
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-03-18 02:18:25 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,19 +17,17 @@ class Author
     @posts << post
     post.author = self
     @@post_count += 1
-
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
     post.author = self
-
+    @@post_count += 1
   end
 
   def self.post_count
-
-
+    @@post_count
   end
 
 
